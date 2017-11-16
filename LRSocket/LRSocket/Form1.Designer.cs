79c23b9bbe17957824554d3b0d390f011d93acfb
schaerenD <daniel.schaeren@gmail.com>
2017-11-16 13:11:06 +0100

    Hinzufügen Testprogramm

---
+++ LRSocket/LRSocket/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace LRSocket
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bConnect = new System.Windows.Forms.Button();
+            this.bDisconnect = new System.Windows.Forms.Button();
+            this.bSend = new System.Windows.Forms.Button();
+            this.lState = new System.Windows.Forms.Label();
+            this.bRead = new System.Windows.Forms.Button();
+            this.lConnction = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // bConnect
+            // 
+            this.bConnect.Location = new System.Drawing.Point(12, 12);
+            this.bConnect.Name = "bConnect";
+            this.bConnect.Size = new System.Drawing.Size(75, 23);
+            this.bConnect.TabIndex = 0;
+            this.bConnect.Text = "Connect";
+            this.bConnect.UseVisualStyleBackColor = true;
+            this.bConnect.Click += new System.EventHandler(this.bConnect_Click);
+            // 
+            // bDisconnect
+            // 
+            this.bDisconnect.Location = new System.Drawing.Point(12, 42);
+            this.bDisconnect.Name = "bDisconnect";
+            this.bDisconnect.Size = new System.Drawing.Size(75, 23);
+            this.bDisconnect.TabIndex = 1;
+            this.bDisconnect.Text = "Disconnect";
+            this.bDisconnect.UseVisualStyleBackColor = true;
+            this.bDisconnect.Click += new System.EventHandler(this.bDisconnect_Click);
+            // 
+            // bSend
+            // 
+            this.bSend.Location = new System.Drawing.Point(12, 72);
+            this.bSend.Name = "bSend";
+            this.bSend.Size = new System.Drawing.Size(75, 23);
+            this.bSend.TabIndex = 2;
+            this.bSend.Text = "Send";
+            this.bSend.UseVisualStyleBackColor = true;
+            this.bSend.Click += new System.EventHandler(this.bSend_Click);
+            // 
+            // lState
+            // 
+            this.lState.AutoSize = true;
+            this.lState.Location = new System.Drawing.Point(12, 159);
+            this.lState.Name = "lState";
+            this.lState.Size = new System.Drawing.Size(16, 13);
+            this.lState.TabIndex = 3;
+            this.lState.Text = "...";
+            // 
+            // bRead
+            // 
+            this.bRead.Location = new System.Drawing.Point(12, 101);
+            this.bRead.Name = "bRead";
+            this.bRead.Size = new System.Drawing.Size(75, 23);
+            this.bRead.TabIndex = 4;
+            this.bRead.Text = "Read";
+            this.bRead.UseVisualStyleBackColor = true;
+            this.bRead.Click += new System.EventHandler(this.bRead_Click);
+            // 
+            // lConnction
+            // 
+            this.lConnction.AutoSize = true;
+            this.lConnction.Location = new System.Drawing.Point(104, 21);
+            this.lConnction.Name = "lConnction";
+            this.lConnction.Size = new System.Drawing.Size(16, 13);
+            this.lConnction.TabIndex = 5;
+            this.lConnction.Text = "...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lConnction);
+            this.Controls.Add(this.bRead);
+            this.Controls.Add(this.lState);
+            this.Controls.Add(this.bSend);
+            this.Controls.Add(this.bDisconnect);
+            this.Controls.Add(this.bConnect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bConnect;
+        private System.Windows.Forms.Button bDisconnect;
+        private System.Windows.Forms.Button bSend;
+        private System.Windows.Forms.Label lState;
+        private System.Windows.Forms.Button bRead;
+        private System.Windows.Forms.Label lConnction;
+    }
+}
+
---
+++ LRSocket/LRSocket/Form1.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace LRSocket
+{
+    public partial class Form1 : Form
+    {
+        LRConnection lr;
+
+        public Form1()
+        {
+            InitializeComponent();
+            lr = new LRConnection(4242, "Localhost");
+
+        }
+
+        private void bConnect_Click(object sender, EventArgs e)
+        {
+            lState.Text = "";
+            try
+            {
+                lr.Connect();
+                lConnction.Text = "Connceted";
+                lConnction.BackColor = Color.LightGreen;
+
+            }
+            catch (Exception ex)
+            {
+                lState.Text = ex.ToString();
+                lConnction.Text = "Conncetion Error";
+                lConnction.BackColor = Color.LightPink;
+                lState.Text = ex.ToString();
+
+            }
+        }
+
+        private void bDisconnect_Click(object sender, EventArgs e)
+        {
+            lState.Text = "";
+            try
+            {
+                //lr.WriteLine("stop");
+                lr.Disconnect();
+                lConnction.Text = "Disconnected";
+                lConnction.BackColor = Color.LightGray;
+
+            }
+            catch (Exception ex)
+            {
+                lConnction.Text = "Disconnection Error";
+                lConnction.BackColor = Color.LightPink;
+                lState.Text = ex.ToString();
+
+                lr.Disconnect();
+            }
+        }
+
+        private void bRead_Click(object sender, EventArgs e)
+        {
+            // lState.Text = l
+            lr.Receive();
+        }
+
+        private void bSend_Click(object sender, EventArgs e)
+        {
+            lr.WriteLine("+");
+        }
+
+      
+    }
+    public class LRConnection
+    {
+        private static TcpClient tcpSocket;
+        private int Port { get; set; }
+        string Host { get; set; }
+
+        private static ManualResetEvent connectDone = new ManualResetEvent(false);
+        private static ManualResetEvent sendDone = new ManualResetEvent(false);
+        private static ManualResetEvent receiveDone = new ManualResetEvent(false);
+
+        private static string response;
+
+        public LRConnection(int port, string hostname)
+        {
+            Port = port;
+            Host = hostname;
+            
+        }
+        public void Connect()
+        {
+            tcpSocket = new TcpClient();
+            tcpSocket.BeginConnect(Host, Port, new AsyncCallback(ConnectCallback), tcpSocket.Client);
+            connectDone.WaitOne();       
+        }
+        private static void ConnectCallback(IAsyncResult ar)
+        {
+            try
+            {
+                Socket client = (Socket)ar.AsyncState;  // Retrieve the socket from the state object.  
+                client.EndConnect(ar);                  // Complete the connection.  
+
+                Console.WriteLine("Socket connected to {0}", client.RemoteEndPoint.ToString());
+                connectDone.Set();                      // Signal that the connection has been made.  
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+        public void Disconnect()
+        {
+            if (IsConnected==true)
+            {
+                tcpSocket.Client.Close();
+                tcpSocket.Close();
+            }
+           
+        }
+        public string Read()
+        {
+            if (IsConnected)
+            {
+                return null;
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            while (tcpSocket.Available > 0)
+            { 
+                int input = tcpSocket.GetStream().ReadByte();
+                sb.Append((char)input);
+            }
+            //System.Threading.Thread.Sleep(TimeOutMs);
+           
+            return sb.ToString();
+        }
+        private void Send(string data)
+        {
+            //if (IsConnected)
+            //{
+            //    byte[] buf = System.Text.ASCIIEncoding.ASCII.GetBytes(data);
+            //    tcpSocket.GetStream().Write(buf, 0, buf.Length);
+            //}
+            //else
+            //{
+            //    throw new Exception("Verbindung ist getrennt!");
+            //}
+
+            byte[] byteData = Encoding.ASCII.GetBytes(data);
+            tcpSocket.Client.BeginSend(byteData, 0, byteData.Length, SocketFlags.None, new AsyncCallback(SendCallback), tcpSocket.Client);
+        }
+        private static void SendCallback(IAsyncResult ar)
+        {
+            try
+            {
+                Socket client = (Socket)ar.AsyncState;  // Retrieve the socket from the state object.  
+                int bytesSent = client.EndSend(ar);     // Complete sending the data to the remote device.  
+                Console.WriteLine("Sent {0} bytes to server.", bytesSent);
+
+
+                sendDone.Set();                         // Signal that all bytes have been sent.  
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        public  void Receive()
+        {
+            try
+            {
+                // Create the state object.
+                StateObject state = new StateObject();
+                state.workSocket = tcpSocket.Client;
+
+                // Begin receiving the data from the remote device.
+                tcpSocket.Client.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+                    new AsyncCallback(ReceiveCallback), state);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+        private static void ReceiveCallback(IAsyncResult ar)
+        {
+            try
+            {
+                // Retrieve the state object and the client socket from the asynchronous state object.
+                StateObject state = (StateObject)ar.AsyncState;
+                Socket client = state.workSocket;
+               
+                int bytesRead = client.EndReceive(ar);  // Read data from the remote device.
+                if (bytesRead > 0)
+                {
+                    state.sb.Append(Encoding.ASCII.GetString(state.buffer, 0, bytesRead));          // There might be more data, so store the data received so far.              
+                    client.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0, new AsyncCallback(ReceiveCallback), state);  //  Get the rest of the data.
+                    Console.WriteLine(state.sb.ToString());
+                }
+                else
+                {
+                    if (state.sb.Length > 1)  // All the data has arrived; put it in response.
+                    {
+                        response = state.sb.ToString();
+                        Console.WriteLine(response);
+                    }
+                    receiveDone.Set();       // Signal that all bytes have been received.
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+
+        public void WriteLine(string cmd)
+        {
+            Send(cmd + "\n");
+        }
+    
+    
+        public bool IsConnected
+        {
+            get
+            {
+                try
+                {
+                    if (tcpSocket != null && tcpSocket.Client != null && tcpSocket.Client.Connected)
+                    {
+                        /* pear to the documentation on Poll:
+                         * When passing SelectMode.SelectRead as a parameter to the Poll method it will return 
+                         * -either- true if Socket.Listen(Int32) has been called and a connection is pending;
+                         * -or- true if data is available for reading; 
+                         * -or- true if the connection has been closed, reset, or terminated; 
+                         * otherwise, returns false
+                         */
+
+                        // Detect if client disconnected
+                        if (tcpSocket.Client.Poll(0, SelectMode.SelectRead))
+                        {
+                            byte[] buff = new byte[1];
+                            if (tcpSocket.Client.Receive(buff, SocketFlags.Peek) == 0)
+                            {
+                                // Client disconnected
+                                return false;
+                            }
+                            else
+                            {
+                                return true;
+                            }
+                        }
+
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+        }
+
+    }
+    public class StateObject
+    {
+        // Client socket.
+        public Socket workSocket = null;
+        // Size of receive buffer.
+        public const int BufferSize = 256;
+        // Receive buffer.
+        public byte[] buffer = new byte[BufferSize];
+        // Received data string.
+        public StringBuilder sb = new StringBuilder();
+    }
+
+
+}
